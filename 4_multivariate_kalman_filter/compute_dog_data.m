f8ec589bf9c296c7538b9c48e6d2dbff535dce97
Luca Maurelli <16053048+LuchiLucs@users.noreply.github.com>
2019-09-27 15:13:45 +0200

    sistemato codice per presentazione

--- 4_multivariate_kalman_filter/compute_dog_data.m
+++ 4_multivariate_kalman_filter/compute_dog_data.m
@@ -9,8 +9,8 @@ function [xs,zs] = compute_dog_data(z_var, process_var, count, dt)
     xs = cell(1,count);
     zs = cell(1,count);
     for ii = 1:count
-        x = x+(x_dot+randn(dim_z,1)*p_std(dim_z))*dt;
+        x = x + (x_dot + randn(dim_z,1) * p_std(dim_z)) *dt;
         xs{ii} = x;
-        zs{ii} = x+randn().*z_std;
+        zs{ii} = x + randn().*z_std;
     end
 end
